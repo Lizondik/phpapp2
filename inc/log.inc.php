83f3d089e4dd15d309f17ac00d370453d5f0302c
Lizondik <lizendik@outlook.com>
2023-01-11 13:44:44 +0600

    add: app

---
+++ inc/log.inc.php
@@ -0,0 +1,12 @@
+<?
+//$dt=date_create();
+//date_timestamp_set($dt,1371803321);
+//$date = format($dt,"U = Y-m-d H:i:s");
+/* $newDate = $dt->format('Y-m-d H:i:s');
+$page = $_SERVER["REQUEST_URI"];
+$ref = $_SERVER["HTTP_REFERER"];
+$path = "$newDate | $page | $ref \n";
+
+$myfile = fopen("PATH_LOG", "w") or die("Unable to create file!");
+fwrite($myfile, $path);
+fclose($myfile); */
